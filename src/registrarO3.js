c70dae38013fc71495641bbdcecf90e506b43e1c
R4tch3t <bebetovic@hotmail.com>
2020-07-31 10:07:15 -0500

    updates

--- src/registrarO3.js
+++ src/registrarO3.js
@@ -1,14 +1,16 @@
 const http = require('http');
 const hostname = '0.0.0.0';
-const port = 3016;
+const port = 3041;
 const mysql = require('mysql');
 
 //const { resolve } = require('path');
 const servers = []
 let bands = [false]
 let ports = 3040
+//const Pdf = require('../renderPDF');
 const regO = require('./regO');
 
+//servers.push(http.createServer(registrarO(0,port)));
 servers.push(http.createServer(regO.regO(servers, 0, port, hostname)));
 servers[servers.length - 1].maxConnections = 1
 servers[0].listen(port, hostname, () => {
@@ -17,7 +19,7 @@ servers[0].listen(port, hostname, () => {
 /*while (servers.length<5){
   const portC = ports
   bands.push(false)
-  servers.push(http.createServer(regO.regO(servers, servers.length, ports, hostname)));
+  servers.push(http.createServer(registrarO(servers.length, ports)));
   servers[servers.length - 1].maxConnections = 1
   servers[servers.length-1].listen(ports, hostname, () => {
     console.log(`Server running ats http://${hostname}:${portC}/`);
