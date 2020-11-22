7231397d81a0f21ef6f8e34a55ef9e47be1ff7fd
root <root@funny-germain.66-175-232-240.plesk.page>
2020-11-22 16:53:45 +0000

    updates

--- src/renderEscritura.js
+++ src/renderEscritura.js
@@ -1,4 +1,4 @@
-var http = require('restify'),
+var http = require('express'),
     port = 2998,
     // phantom = require('phantom'),
     //tmpdir = require('os').tmpdir(),
@@ -23,15 +23,16 @@ try {
 }
 var server = http.createServer(options);
 
-server.get('/escrituras/:tp/:CTA/:escritura', function(req, res, next) {
+server.get('/expediente/:tp/:CTA/:escritura', function(req, res, next) {
     try {
-        var filename = "escrituras/" + req.params.tp + "/" + req.params.CTA
+        var filename = "/var/expedientes/" + req.params.tp + "/" + req.params.CTA
         filename += "/" + req.params.escritura
             //filename = path.join(__dirname, filename)
             // console.log(filename)
             // file = tmpdir + filename;
         setResponseHeaders(res, req.params.escritura);
-        fs.createReadStream(path.join(__dirname, filename)).pipe(res);
+        //fs.createReadStream(path.join(__dirname, filename)).pipe(res);
+        fs.createReadStream(filename).pipe(res);
         /*
         //To Create in the fly pdf
         (async function() {
